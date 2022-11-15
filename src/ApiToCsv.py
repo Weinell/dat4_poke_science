f4d5e2424d3613a781818d28a413c00cb61ff9a6
Nikolaj Weinell <nikolajweinell@hotmail.com>
2022-11-15 11:55:36 +0100

    More focused data

--- src/ApiToCsv.py
+++ src/ApiToCsv.py
@@ -6,24 +6,24 @@ def get_pokemon(name: str):
     
     data = res.json()
 
-    types = data['types']
+    name = data['name']
+    poke_id = data['id']
 
-    type1 = data['types'][0]
-    type2 = data['types'][1] if len(data['types'])>1 else None
+    type1 = data['types'][0]['type']['name']
+    type2 = data['types'][1]['type']['name'] if len(data['types'])>1 else None
 
+    hp = data['stats'][0]['base_stat']
+    attack = data['stats'][1]['base_stat']
+    defense = data['stats'][2]['base_stat']
+    sp_attack = data['stats'][3]['base_stat']
+    sp_defense = data['stats'][4]['base_stat']
+    speed = data['stats'][5]['base_stat']
 
-    hp = data['stats'][0]
-    attack = data['stats'][1]
-    defense = data['stats'][2]
-    sp_attack = data['stats'][3]
-    sp_defense = data['stats'][4]
-    speed = data['stats'][5]
-
-    print(type1)
-    print(type2)
+    return poke_id, name, type1, type2, hp, attack, defense, sp_attack, sp_defense, speed
 
     
 
 if __name__ == '__main__':
     get_pokemon('pikachu')
     get_pokemon('charizard')
+    print(get_pokemon('pikachu'))
