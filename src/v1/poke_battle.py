34538e9f1b802926a0d871ab59f08fe9bc8e26fa
Freddiiy <frederikgaller@live.dk>
2022-11-16 16:29:11 +0100

    asd

---
+++ src/v1/poke_battle.py
@@ -0,0 +1,30 @@
+import math
+import os.path
+import random
+import pandas as pd
+from src.ApiToCsv import read_pokemon_data
+
+MAX_ID = 905
+
+
+def get_random_pokemon(not_this_pokemon: int | None):
+    poke_id = math.floor(random.random() * MAX_ID) + 1
+    print(poke_id)
+
+
+def battle_pokemon(first: int, second: int):
+    df = read_pokemon_data()
+
+    poke_one = df[df["Id"] == first]
+    poke_two = df[df["Id"] == second]
+
+    poke_one.astype({"Sum_stats": "int"})
+    
+    if poke_one.Sum_stats > poke_two.Sum_stats:
+        print(f"{poke_one} wins")
+    else:
+        print(f"{poke_two} wins")
+
+
+if __name__ == "__main__":
+    battle_pokemon(4, 25)
