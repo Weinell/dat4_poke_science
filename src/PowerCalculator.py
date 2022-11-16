0149912a0685b4c18fda0686923ea17112409212
Nikolaj Weinell <nikolajweinell@hotmail.com>
2022-11-16 14:42:49 +0100

    Add sum_stats

--- src/PowerCalculator.py
+++ src/PowerCalculator.py
@@ -6,3 +6,12 @@ df = pd.read_csv(data)
 print(df)
 
 
+print(df.columns)
+
+def sum_stats():
+    for index in df.index:
+        df['Sum_Stats'] = df.apply(lambda row: row.HP + row.Attack + row.Defense + row.SpAttack + row.SpDefense + row.Speed,axis=1)
+
+sum_stats()
+print(df)
+
