997e6d1d949f4ddd632d549825186474a791f90d
bønne <mikkel_denseje@live.dk>
2022-11-16 17:56:27 +0100

    write match csv

--- src/v1/poke_battle.py
+++ src/v1/poke_battle.py
@@ -1,3 +1,4 @@
+import csv
 import math
 import os.path
 import random
@@ -21,20 +22,33 @@ def battle_pokemon(first: int, second: int):
     poke_one = df[df["Id"] == first]
     poke_two = df[df["Id"] == second]
 
+    print(poke_two.values[0])
+
     poke_one_stats = poke_one["Sum_stats"].values[0]
     poke_two_stats = poke_two["Sum_stats"].values[0]
 
     if poke_one_stats > poke_two_stats:
-        winner = poke_one
-        loser = poke_two
+        winner = poke_one.values[0]
+        loser = poke_two.values[0]
         stat_diff = poke_one_stats - poke_two_stats
     else:
-        winner = poke_two
-        loser = poke_one
+        winner = poke_two.values[0]
+        loser = poke_one.values[0]
         stat_diff = poke_two_stats - poke_one_stats
 
     return winner, loser, stat_diff
 
 
+def write_match_details():
+    header = ['Winner', 'Loser', 'stat_diff']
+
+    match = battle_pokemon(3, 25)
+
+    with open("../../data/v1/match.csv", "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerow(match)
+
+
 if __name__ == "__main__":
-    battle_pokemon(3, 25)
+    write_match_details()
