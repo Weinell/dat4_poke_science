a479ad4fa991c8830fd25ee8c5919dcb7df8b238
bønne <mikkel_denseje@live.dk>
2022-11-16 17:36:31 +0100

    pokemon battle with stats work

--- src/v1/poke_battle.py
+++ src/v1/poke_battle.py
@@ -14,22 +14,27 @@ def get_random_pokemon(not_this_pokemon: int | None):
 
 def battle_pokemon(first: int, second: int):
     df = read_pokemon_data()
+    global winner
+    global loser
+    global stat_diff
 
     poke_one = df[df["Id"] == first]
     poke_two = df[df["Id"] == second]
 
+    poke_one_stats = poke_one["Sum_stats"].values[0]
+    poke_two_stats = poke_two["Sum_stats"].values[0]
 
+    if poke_one_stats > poke_two_stats:
+        winner = poke_one
+        loser = poke_two
+        stat_diff = poke_one_stats - poke_two_stats
+    else:
+        winner = poke_two
+        loser = poke_one
+        stat_diff = poke_two_stats - poke_one_stats
 
-    print(poke_one["Sum_stats"])
-    print(poke_two["Sum_stats"])
-
-    #poke_one.astype({"Sum_stats": "int"})
-    
-   #if poke_one.Sum_stats > poke_two.Sum_stats:
-       # print(f"{poke_one} wins")
-   #else:
-       # print(f"{poke_two} wins")
+    return winner, loser, stat_diff
 
 
 if __name__ == "__main__":
-    battle_pokemon(4, 25)
+    battle_pokemon(3, 25)
