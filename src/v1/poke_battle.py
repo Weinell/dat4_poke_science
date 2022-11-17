5decfeb56c25bb3e2e9e3528ebbe345796b0a1c7
Freddiiy <frederikgaller@live.dk>
2022-11-17 14:30:04 +0100

    changes to how we store matches

--- src/v1/poke_battle.py
+++ src/v1/poke_battle.py
@@ -6,31 +6,26 @@ MAX_ID = 151
 
 def battle_pokemon(first: int, second: int):
     df = read_pokemon_data()
-    global winner
-    global loser
-    global stat_diff
-
     poke_one = df[df["Id"] == first]
     poke_two = df[df["Id"] == second]
+
+    poke_one_id = df[df["Id"] == first].values[:, 0][0]
+    poke_two_id = df[df["Id"] == second].values[:, 0][0]
 
     poke_one_stats = poke_one["Sum_stats"].values[0]
     poke_two_stats = poke_two["Sum_stats"].values[0]
 
     if poke_one_stats > poke_two_stats:
-        winner = poke_one.values[0]
-        loser = poke_two.values[0]
-        stat_diff = poke_one_stats - poke_two_stats
+        winner = poke_one_id
     else:
-        winner = poke_two.values[0]
-        loser = poke_one.values[0]
-        stat_diff = poke_two_stats - poke_one_stats
+        winner = poke_two_id
 
-    return winner, loser, stat_diff
+    return poke_one_id, poke_two_id, winner
 
 
 def write_all_battles():
     poke_count = MAX_ID
-    header = ['Winner', 'Loser', 'stat_diff']
+    header = ['Poke_1', 'Poke_2', 'Winner']
 
     with open("../../data/v1/match.csv", "w") as file:
         writer = csv.writer(file)
@@ -44,3 +39,4 @@ def write_all_battles():
 
 if __name__ == "__main__":
     write_all_battles()
+    #battle_pokemon(4, 25)
