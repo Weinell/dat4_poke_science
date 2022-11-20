d55a3475cdb9f97bafb6e187226cc50c1fcf63d8
Nikolaj <nikolajweinell@hotmail.com>
2022-11-20 12:55:36 +0100

    Battle data can be written to csv file (relatively fast)

--- src/v1/poke_battle.py
+++ src/v1/poke_battle.py
@@ -2,6 +2,7 @@ import csv
 import time
 from src.ApiToCsv import read_pokemon_data, read_battles_data
 import numpy as np
+import pandas as pd
 
 MAX_ID = 151
 
@@ -43,13 +44,34 @@ def calc_battles():
     pokemons = read_pokemon_data("../../data/pokemon.csv")
     battle_results = read_battles_data("../../data/v1/match.csv")
 
+    #header = ['Poke_1_Id', 'Poke_1_Name', 'Poke_1_HP', 'Poke_1_Attack', 'Poke_1_Defense', 'Poke_1_Sp_Attack',
+     #         'Poke_1_Sp_Defense', 'Poke_1_Speed', 'Poke_1_Sum_Stats',
+     #         'Poke_2_Id', 'Poke_2_Name', 'Poke_2_HP', 'Poke_2_Attack', 'Poke_2_Defense', 'Poke_2_Sp_Attack',
+     #         'Poke_2_Sp_Defense', 'Poke_2_Speed', 'Poke_2_Sum_Stats']
+    #'Diff_HP', 'Diff_Attack', 'Diff_Defense', 'Diff_Sp_Attack', 'Diff_Sp_Defense', 'Diff_Speed', 'Diff_Sum_Stats'
+
+    header = ['Winner','Poke_1_HP', 'Poke_1_Attack', 'Poke_1_Defense', 'Poke_1_Sp_Attack','Poke_1_Sp_Defense','Poke_1_Speed','Poke_2_HP', 'Poke_2_Attack', 'Poke_2_Defense', 'Poke_2_Sp_Attack','Poke_2_Sp_Defense', 'Poke_2_Speed']
+
+    df = pd.DataFrame()
+
     for index, row in battle_results.iterrows():
         first_pokemon = row["Poke_1"]
         second_pokemon = row["Poke_2"]
+
         winner = row["Winner"]
 
-        poke_one = pokemons.loc[pokemons["Id"] == first_pokemon].values[:, 4][0]
-        poke_two = pokemons.loc[pokemons["Id"] == second_pokemon].values[:, 4][0]
+        poke_one = pokemons[pokemons["Id"] == first_pokemon]
+        poke_two = pokemons[pokemons["Id"] == second_pokemon]
+
+        pokemon_battle_info = pd.Series([winner,poke_one['HP'].values[0],poke_one['Attack'].values[0],poke_one['Defense'].values[0],poke_one['Sp_Attack'].values[0],poke_one['Sp_Defense'].values[0],poke_one['Speed'].values[0],poke_two['HP'].values[0],poke_two['Attack'].values[0],poke_two['Defense'].values[0],poke_two['Sp_Attack'].values[0],poke_two['Sp_Defense'].values[0],poke_two['Speed'].values[0]])
+        battle_to_concat = pd.DataFrame([pokemon_battle_info])
+        df = pd.concat([battle_to_concat, df], ignore_index=True)
+        print('columns:',df.columns)
+        print(df)
+
+    print(df)
+    df.to_csv('battle_data.csv', index=False)
+    print(df)
 
 
 if __name__ == "__main__":
