7a1f8818ae8a99fd6a9eff2615c9e5b87894c7de
Mikkel Bentsen <bentsen405@gmail.com>
2022-11-21 21:28:13 +0100

    Feature/v1 machine learning (#15)

--- src/v1/knn.py
+++ src/v1/knn.py
@@ -1,31 +1,24 @@
-import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn import linear_model
+import pandas as pd
+
+df = pd.read_csv("battle_data.csv")
+
+y = df.iloc[:, :1]
+X = df.iloc[:, 1:]
 
 
-df = pd.read_csv('battle_data_diff.csv')
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.9, random_state=42)
 
-X = df.iloc[:,1:]
-y = df.iloc[:,:1]
 
-# Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
 
-# Scale data
-SC = StandardScaler()
-X_train = SC.fit_transform(X_train)
-X_test = SC.transform(X_test)
+score = knn.score(X_test, y_test)
 
-# Ridge regression
-reg = linear_model.Ridge(alpha=0.5)
-reg.fit(X_train, y_train)
 
-pred = reg.predict(X_test)
-score = reg.score(X_test, y_test)
+if __name__ == "__main__":
+    print(score)
 
-if __name__ == '__main__':
-    print(reg.coef_)
-    print(reg.intercept_)
-    print('preds', pred)
-    print('score', score)
+
+
